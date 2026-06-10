efe6ca502ad3fce0da3852807de7c41692f4d177
Jack Snyder <108773771+zquify@users.noreply.github.com>
2026-06-10 10:46:03 -0600

    Add variable-value food items
    Added support for food items with different score values. Larger or more substantial foods can now be worth more points than smaller ones. Added bananas as a higher-value food type.

--- src/anthill/anthill.gd
+++ src/anthill/anthill.gd
@@ -12,10 +12,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	score = get_colliders_in_group("carryable")
+	score = get_score("carryable")
 	$Label3D.text = str(score)
 
-func get_colliders_in_group(group_name: String) -> int:
+func get_score(group_name: String) -> int:
 	var count = 0
 	# Retrieve all physics bodies currently overlapping the area
 	var overlapping_bodies = area_3d.get_overlapping_bodies()
@@ -23,6 +23,6 @@ func get_colliders_in_group(group_name: String) -> int:
 	for body in overlapping_bodies:
 		# Check if the body belongs to your target group
 		if body.is_in_group(group_name):
-			count += 1
+			count += body.score
 			
 	return count
--- src/pickup/carryable.gd
+++ src/pickup/carryable.gd
@@ -2,10 +2,10 @@ extends RigidBody3D
 
 @onready var starting_transform: Transform3D = self.global_transform
 
+@export var score: int = 1
+
 func return_home():
 	linear_velocity = Vector3.ZERO
 	angular_velocity = Vector3.ZERO
-
-	sleeping = true
 
 	global_transform = starting_transform
