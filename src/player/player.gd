562c161c50356b35e69dfb7966306b3e5c1a2d5d
Jack Snyder <108773771+zquify@users.noreply.github.com>
2026-06-15 19:10:44 -0600

    Update player.gd

--- src/player/player.gd
+++ src/player/player.gd
@@ -111,6 +111,9 @@ func _input(event: InputEvent) -> void:
 	if in_menu:
 		return
 	
+	if Input.is_action_just_pressed("esc"):
+		get_tree().quit()
+	
 	if event is InputEventMouseMotion:
 		yaw   -= event.relative.x * mouseSens
 		pitch -= event.relative.y * mouseSens
